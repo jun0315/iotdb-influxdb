2cda9979b0f9ceb1d9e199a48edff218857bf97d
谢其骏 <xieqijun@mail.nwpu.edu.cn>
2021-07-31 17:01:03 +0800

    feat:add test example

--- src/test/java/org/apache/iotdb/influxdb/IotDBInfluxDBTest.java
+++ src/test/java/org/apache/iotdb/influxdb/IotDBInfluxDBTest.java
@@ -1,85 +1,134 @@
 package org.apache.iotdb.influxdb;
 
 import org.apache.iotdb.infludb.IotDBInfluxDB;
+import org.apache.iotdb.rpc.IoTDBConnectionException;
+import org.apache.iotdb.rpc.StatementExecutionException;
+import org.apache.iotdb.session.Session;
+import org.apache.iotdb.session.SessionDataSet;
+import org.apache.iotdb.tsfile.file.metadata.enums.TSDataType;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class IotDBInfluxDBTest {
     private IotDBInfluxDB iotDBInfluxDB;
 
+
+    private static final String LOCAL_HOST = "127.0.0.1";
+    private static Session session;
+    private static final String ROOT_T1 = "root.teststress.test1";
+    private static final String ROOT_T2 = "root.teststress.test2";
+
     @Before
-    public void setUp() {
+    public void setUp() throws IoTDBConnectionException {
         //创建 连接
-        iotDBInfluxDB = new IotDBInfluxDB();
+        session = new Session(LOCAL_HOST, 6667, "root", "root");
+        session.open(false);
+
+        // set session fetchSize
+        session.setFetchSize(10000);
     }
 
     @Test
-    public void testInsert() {//测试数据插入
-//        Map<String, String> tags = new HashMap<String, String>();
-//        Map<String, Object> fields = new HashMap<String, Object>();
-//        List<CodeInfo> list = new ArrayList<CodeInfo>();
-//
-//        CodeInfo info1 = new CodeInfo();
-//        info1.setId(1L);
-//        info1.setName("BANKS");
-//        info1.setCode("ABC");
-//        info1.setDescr("中国农业银行");
-//        info1.setDescrE("ABC");
-//        info1.setCreatedBy("system");
-//        info1.setCreatedAt(new Date().getTime());
-//
-//        CodeInfo info2 = new CodeInfo();
-//        info2.setId(2L);
-//        info2.setName("BANKS");
-//        info2.setCode("CCB");
-//        info2.setDescr("中国建设银行");
-//        info2.setDescrE("CCB");
-//        info2.setCreatedBy("system");
-//        info2.setCreatedAt(new Date().getTime());
-//
-//        list.add(info1);
-//        list.add(info2);
-//
-//        for (CodeInfo info : list) {
-//
-//            tags.put("TAG_CODE", info.getCode());
-//            tags.put("TAG_NAME", info.getName());
-//
-//            fields.put("ID", info.getId());
-//            fields.put("NAME", info.getName());
-//            fields.put("CODE", info.getCode());
-//            fields.put("DESCR", info.getDescr());
-//            fields.put("DESCR_E", info.getDescrE());
-//            fields.put("CREATED_BY", info.getCreatedBy());
-//            fields.put("CREATED_AT", info.getCreatedAt());
-//
-//            influxDB.insert(measurement, tags, fields);
-//        }
+    public void testInsert1() throws IoTDBConnectionException, StatementExecutionException {//测试数据插入
+        String deviceId = ROOT_T1;
+        int fieldNum = 100;
+        int tagNum = 10;
+        List<String> measurements = new ArrayList<>();
+        List<TSDataType> types = new ArrayList<>();
+        for (int i = 0; i < fieldNum; i++) {
+            measurements.add(util.generateWord(i));
+            types.add(TSDataType.INT64);
+        }
+        for (int i = 0; i < tagNum; i++) {
+            int baseline = 300;
+            String tmpTag = util.generateWord(i + baseline);
+            System.out.println("tag:" + tmpTag);
+            measurements.add(tmpTag);
+            types.add(TSDataType.INT64);
+        }
+
+        for (long time = 0; time < 2000; time++) {
+            List<Object> values = new ArrayList<>();
+            for (long i = 0; i < fieldNum + tagNum; i++) {
+                values.add(time);
+            }
+            session.insertRecord(deviceId, time, measurements, types, values);
+        }
     }
 
     @Test
-    public void testQuery() {//测试数据查询
-//        String command = "select * from sys_code";
-//        QueryResult results = influxDB.query(command);
-//
-//        if (results.getResults() == null) {
-//            return;
-//        }
-//        List<CodeInfo> lists = new ArrayList<CodeInfo>();
-//        for (Result result : results.getResults()) {
-//
-//            List<Series> series = result.getSeries();
-//            for (Series serie : series) {
-////				Map<String, String> tags = serie.getTags();
-//                List<List<Object>> values = serie.getValues();
-//                List<String> columns = serie.getColumns();
-//
-//                lists.addAll(getQueryData(columns, values));
-//            }
-//        }
-//
-//        Assert.assertTrue((!lists.isEmpty()));
-//        Assert.assertEquals(2, lists.size());
+    public void testInsert2() throws IoTDBConnectionException, StatementExecutionException {//测试数据插入
+        String deviceId = ROOT_T2;
+        int fieldNum = 100;
+        int tagNum = 10;
+        for (int i = 0; i < tagNum; i++) {
+            List<String> measurements = new ArrayList<>();
+            List<TSDataType> types = new ArrayList<>();
+            int baseline = 300;
+            String tmpTag = util.generateWord(i + baseline);
+            deviceId += "." + tmpTag;
+            for (int j = 0; j < fieldNum; j++) {
+                measurements.add(util.generateWord(j));
+                types.add(TSDataType.INT64);
+            }
+
+            for (long time = 0; time < 200; time++) {
+                List<Object> values = new ArrayList<>();
+                for (long k = 0; k < fieldNum; k++) {
+                    values.add(time);
+                }
+                session.insertRecord(deviceId, time, measurements, types, values);
+            }
+        }
     }
 
+
+    @Test
+    public void testQuery1() throws IoTDBConnectionException, StatementExecutionException {//测试数据查询
+
+        long before = System.currentTimeMillis();
+        SessionDataSet dataSet = session.executeQueryStatement("select * from root.teststress.test1 where RL = 1 and A = 1 and B =1 and C=1");
+        long after = System.currentTimeMillis();
+        long duration = (after - before);
+        System.out.println(duration);
+//        System.out.println(dataSet.getColumnNames());
+//        dataSet.setFetchSize(1024); // default is 10000
+//        int index = 0;
+//        while (dataSet.hasNext()) {
+////            System.out.println(dataSet.next());
+//            index++;
+//        }
+//        System.out.println(index);
+
+//        dataSet.closeOperationHandle();
+    }
+
+    @Test
+    public void testQuery2() throws IoTDBConnectionException, StatementExecutionException {//测试数据查询
+        long before = System.currentTimeMillis();
+        SessionDataSet dataSet = session.executeQueryStatement("select * from root.teststress.test2.*.*.*.*.SL where A=1 and B=1 and C=1");
+        dataSet = session.executeQueryStatement("select * from root.teststress.test2.*.*.*.*.SL.* where A=1 and B=1 and C=1");
+        dataSet = session.executeQueryStatement("select * from root.teststress.test2.*.*.*.*.SL.*.*.* where A=1 and B=1 and C=1");
+        dataSet = session.executeQueryStatement("select * from root.teststress.test2.*.*.*.*.SL.*.*.*.* where A=1 and B=1 and C=1");
+        dataSet = session.executeQueryStatement("select * from root.teststress.test2.*.*.*.*.SL.*.*.*.*.* where A=1 and B=1 ");
+        dataSet = session.executeQueryStatement("select * from root.teststress.test2.* where A=1 and B=1 ");
+        long after = System.currentTimeMillis();
+        long duration = (after - before);
+        System.out.println(duration);
+//        System.out.println(dataSet.getColumnNames());
+//        dataSet.setFetchSize(1024); // default is 10000
+//        int index = 0;
+//        while (dataSet.hasNext()) {
+////            System.out.println(dataSet.next());
+//            index++;
+//        }
+//        System.out.println(index);
+//
+//        dataSet.closeOperationHandle();
+    }
 }
